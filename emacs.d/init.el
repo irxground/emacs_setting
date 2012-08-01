100a0767ee75d56b2b6e56103b73173cfecb70d6
irxground <irxnjhtchlnrw@gmail.com>
2012-08-01 10:14:10 +0900

    Update emacs setting

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -18,7 +18,14 @@
 (cua-mode t)             ; use Common-User-Access mode
 (setq inhibit-startup-screen t)
 
-(mouse-wheel-mode)
+(mouse-wheel-mode t)
+(setq mouse-wheel-progressive-speed nil)
+(progn
+  (global-set-key [wheel-left]  '(lambda() "" (interactive) (scroll-left  1)))
+  (global-set-key [wheel-right] '(lambda() "" (interactive) (scroll-right 1))))
+
+
+(if nil (progn
 (global-set-key [wheel-up]
                 '(lambda () "" (interactive) (scroll-down 1)))
 (global-set-key [wheel-down]
@@ -31,4 +38,66 @@
                 '(lambda () "" (interactive) (scroll-down 2)))
 (global-set-key [triple-wheel-down]
                 '(lambda () "" (interactive) (scroll-up 2)))
+) (interactive))
+
+;; suppress trancate lines
+(defun toggle-truncate-lines ()
+  (interactive)
+  (if truncate-lines
+    (setq truncate-lines nil)
+    (setq truncate-lines t))
+  (recenter))
+(global-set-key "\C-c\C-l" 'toggle-truncate-lines)
+
+
+;; split window
+(global-set-key (kbd "s-d")   'split-window-horizontally)
+(global-set-key (kbd "s-D") 'split-window-vertically)
+
+;; window move
+(global-set-key (kbd "<M-s-up>")    'windmove-up)
+(global-set-key (kbd "<M-s-down>")  'windmove-down)
+(global-set-key (kbd "<M-s-left>")  'windmove-left)
+(global-set-key (kbd "<M-s-right>") 'windmove-right)
+
+;; theme
+(custom-set-faces
+  '(default ((t (:background "black" :foreground "white"))))
+  '(cursor (
+            (((class color) (background dark))  (:background "#55FF55"))
+            (((class color) (background light)) (:background "#999999"))
+            (t ())
+)))
+
+
+;; for trailing whitespace
+(setq-default tab-width 2 indent-tabs-mode nil)
+
+;;(defface my-face-r-1 '((t (:background "gray15"))) nil)
+(defface my-face-b-1 '((t (:background "gray"))) nil)
+(defface my-face-b-2 '((t (:background "gray20"))) nil)
+(defface my-face-u-1 '((t (:background "#700"))) nil)
+;;(defvar my-face-r-1 'my-face-r-1)
+(defvar my-face-b-1 'my-face-b-1)
+(defvar my-face-b-2 'my-face-b-2)
+(defvar my-face-u-1 'my-face-u-1)
+
+(defadvice font-lock-mode (before my-font-lock-mode ())
+  (font-lock-add-keywords
+   major-mode
+   '(("\t" 0 my-face-b-2 append)
+     ("　" 0 my-face-b-1 append)
+     ("[ \t]+$" 0 my-face-u-1 append)
+     ;;("[\r]*\n" 0 my-face-r-1 append)
+     )))
+(ad-enable-advice 'font-lock-mode 'before 'my-font-lock-mode)
+(ad-activate 'font-lock-mode)
+
+
+;; indent when push key enter
+
+(global-set-key (kbd "<RET>") 'reindent-then-newline-and-indent)
+
+(require 'ruby-mode)
+(define-key ruby-mode-map (kbd "<RET>") 'reindent-then-newline-and-indent)
 
